561fd3980136b13c2b1ade863e08458fa32904af
Seano <sean01234@yahoo.com>
2023-07-02 00:19:14 -0400

    remove temp folder

--- temp/temp2/src/api/authorApi.js
+++
@@ -1,8 +0,0 @@
-import {handleResponse,handleError} from "./apiUtils";
-const baseUrl=process.env.API_URL+"/priorities/";
-
-export function getPriorities() {
-  return fetch(baseUrl)
-    .then(handleResponse)
-    .catch(handleError);
-}
--- temp/temp2/src/redux/reducers/initialState.js
+++
@@ -1,5 +0,0 @@
-export default {
-  topics: [],
-  priorities: [],
-  apiCallsInProgress: 0
-};
